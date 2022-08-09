52e394b2ded03ac616a1685f440cf66bd3860f60
Haley Seymour <haleycseymour@comcast.net>
2022-08-08 21:47:43 -0700

    mysql queries x2

--- script.js
+++ script.js
@@ -1391,3 +1391,12 @@ function getMoneySpent(keyboards, drives, b) {
 // +++++++++++++++++++++++
 // +++++++++++++++++++++++
 // +++++++++++++++++++++++
+
+
+// SQL PROBLEMS
+
+// Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
+// SELECT * FROM CITY WHERE POPULATION > 100000 AND COUNTRYCODE = 'USA'
+
+// Query the NAME field for all American cities in the CITY table with populations larger than 120000. The CountryCode for America is USA.
+// SELECT NAME FROM CITY WHERE COUNTRYCODE = 'USA' AND POPULATION > 120000
